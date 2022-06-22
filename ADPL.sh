b21e1dff7a26fd0c23992272fd841ba2189779e9
Kat <47306585+KittyCatOfMagic27@users.noreply.github.com>
2022-06-22 17:53:27 -0500

    BETTER FLAGS

--- ADPL.sh
+++ ADPL.sh
@@ -1,32 +1,16 @@
 
 
 function adpl() {
-  if [ $1 = "--help" ]
+  if [ $1 = "--build" ]
   then
-    echo ""
-    echo "adpl --help             brings up this help page."
-    echo "adpl -b <file>.adpl     Recompiles the compiler and then compiles adpl code from adpl file."
-    echo "adpl -d <file>.adpl     Prints debug text while compiling."
-    echo ""
-    echo ""
-    echo ""
-    echo "adpl(1)                 Compiles adpl code from adpl file."
-    echo ""
-    echo "For additional information check INFO.txt in the ADPL folder."
- 	elif [ $1 = "-b" ]
-  then
+    echo "Building..."
     g++ /home/$USER/ADPL/adpl.cpp -o /home/$USER/ADPL/adpl_compiler
-    if [ $1 = "-d" ]
-    then
-      /home/$USER/ADPL/./adpl_compiler $3 $PWD 1 # disfunctional, don't know why and don't care enough to fix
-    else
-      /home/$USER/ADPL/./adpl_compiler $2 $PWD 0 
-    fi
-  elif [ $1 = "-d" ]
-  then
-    /home/$USER/ADPL/./adpl_compiler $2 $PWD 1
+    echo "Done Building!"
   else
-    /home/$USER/ADPL/./adpl_compiler $1 $PWD 0
+    args=("$@")
+    ELEMENTS=${#args[@]}
+    echo "$@"
+    /home/$USER/ADPL/./adpl_compiler "$@"
   fi
   return 69
 }
