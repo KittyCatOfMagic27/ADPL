57d866cf5275f62575db0b815bf6734a1b1d69f3
Kat <47306585+KittyCatOfMagic27@users.noreply.github.com>
2022-06-22 18:16:51 -0500

    Update ADPL.sh

--- ADPL.sh
+++ ADPL.sh
@@ -7,9 +7,6 @@ function adpl() {
     g++ /home/$USER/ADPL/adpl.cpp -o /home/$USER/ADPL/adpl_compiler
     echo "Done Building!"
   else
-    args=("$@")
-    ELEMENTS=${#args[@]}
-    echo "$@"
     /home/$USER/ADPL/./adpl_compiler "$@"
   fi
   return 69
